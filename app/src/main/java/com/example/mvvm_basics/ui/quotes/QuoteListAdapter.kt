f6fd57ff93cfb300a58a908a011609f272a638f6
salomirc <36732879+salomirc@users.noreply.github.com>
2020-02-06 20:08:18 +0200

    SetOnTouchListeners().

--- app/src/main/java/com/example/mvvm_basics/ui/quotes/QuoteListAdapter.kt
+++ app/src/main/java/com/example/mvvm_basics/ui/quotes/QuoteListAdapter.kt
@@ -1,16 +1,19 @@
 package com.example.mvvm_basics.ui.quotes
 
 import android.content.Context
+import android.graphics.Color
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mvvm_basics.R
 import com.example.mvvm_basics.data.Quote
 import kotlinx.android.synthetic.main.recyclerview_item.view.*
 
-class QuoteListAdapter(context: Context,
+class QuoteListAdapter(private val context: Context,
                        private var quotes : MutableList<Quote>,
                        private val itemClick: (Quote, Int, QuoteListAdapter) -> Unit) :
     RecyclerView.Adapter<QuoteListAdapter.QuoteListHolder>() {
@@ -30,17 +33,27 @@ class QuoteListAdapter(context: Context,
     override fun onBindViewHolder(holder: QuoteListHolder, position: Int) {
         val current = quotes[position]
         holder.quoteItemTextView.text = current.toString()
-        holder.quoteItemTextView.setBackgroundResource(
-            if (current.isSelected ) R.color.itemTextViewBgColorSelected else R.color.itemTextViewBgColorDefault
-        )
+//        holder.quoteItemTextView.setBackgroundResource(
+//            if (current.isSelected ) R.color.itemTextViewBgColorSelected else R.color.itemTextViewBgColorDefault
+//        )
         holder.view.setOnClickListener {
             itemClick(current, position, this)
+        }
+
+        holder.view.setOnTouchListener { v, event ->
+            if (event.action == MotionEvent.ACTION_DOWN){
+                v.setBackgroundColor(ContextCompat.getColor(context, R.color.itemTextViewBgColorSelected))
+            }
+            if (event.action == MotionEvent.ACTION_UP || event.action == MotionEvent.ACTION_CANCEL){
+                v.setBackgroundColor(Color.TRANSPARENT)
+            }
+            return@setOnTouchListener false
         }
     }
 
     override fun onViewRecycled(holder: QuoteListHolder) {
         super.onViewRecycled(holder)
-        holder.quoteItemTextView.setBackgroundResource(R.color.itemTextViewBgColorDefault)
+        holder.quoteItemTextView.setBackgroundColor(Color.TRANSPARENT)
     }
 
     internal fun setWords(quotes: MutableList<Quote>) {
